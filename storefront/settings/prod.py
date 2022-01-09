11b091824dadacd545616909daf1c7a804f4ed0a
CeereeC <myself3216@gmail.com>
2022-01-10 00:45:51 +0800

    Configs

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,6 +1,7 @@
 from storefront.settings.dev import SECRET_KEY
 from .common import *
 import os
+import dj_database_url
 
 DEBUG = False
 
@@ -8,3 +9,6 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['backend-e-comm.herokuapp.com']
 
+DATABASES = {
+    'default': dj_database_url.config()
+}
